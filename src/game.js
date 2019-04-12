ab11e0430a52d5d57c0f9ca4638ab70106b48aed
Scott Mallinson <scott@scottmallinson.com>
2019-04-12 17:39:14 +0200

    Created game loop and basic game logic

--- src/game.js
+++ src/game.js
@@ -1,3 +1,73 @@
 'use strict';
 
 console.log('GAME CONNECTED');
+
+function Game(canvas){
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext('2d');
+  this.duration = 130; // length of song
+  this.currentPosition = 0; // position in song
+  this.player;
+  this.instrument;
+  this.score = 0;
+  this.streak = 0;
+  this.isWin = false;
+  this.isGameOver = false;
+}
+
+Game.prototype.startLoop = function() {
+  const loop = () => {
+    this.clearCanvas();
+    this.updateCanvas();
+    this.drawCanvas();
+    this.checkDuration();
+    this.checkCollisions();
+
+    this.currentPosition++; // MOVE INTO checkDuration();
+
+    if (this.isGameOver === false){
+      window.requestAnimationFrame(loop);
+    }
+    console.log(this.isGameOver);
+  }
+
+  window.requestAnimationFrame(loop);
+}
+
+Game.prototype.clearCanvas = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+Game.prototype.updateCanvas = function() {
+
+}
+
+Game.prototype.drawCanvas = function() {
+  
+}
+
+Game.prototype.checkDuration = function() {
+  if (this.currentPosition < this.duration){
+    this.isGame = false;
+  }
+  else if ((this.currentPosition >= this.duration) && (this.score === 0)){
+    this.isGameOver = true;
+    this.buildGameOverScreen();
+  }
+  else {
+    this.isWin = true;
+    this.buildWinScreen();
+  }
+}
+
+Game.prototype.checkCollisions = function() {
+
+}
+
+Game.prototype.setGameOverCallback = function(buildGameOverScreen) {
+  this.buildGameOverScreen = buildGameOverScreen;
+}
+
+Game.prototype.setWinCallback = function(buildWinScreen) {
+  this.buildWinScreen = buildWinScreen;
+}
--- src/main.js
+++ src/main.js
@@ -2,73 +2,78 @@
 
 console.log('MAIN CONNECTED');
 
-const mainElement = document.querySelector('main');
+function main(){
 
-function buildDom(html) {
-  mainElement.innerHTML = html;
-  return mainElement;
+  const mainElement = document.querySelector('main');
+
+  function buildDom(html) {
+    mainElement.innerHTML = html;
+    return mainElement;
+  }
+
+  function updateTitle(name) {
+    document.title = name;
+  }
+
+  function buildSplash() {
+    const title = updateTitle(`Game name`);
+    const buildSplash = buildDom(`
+    <section>
+      <h1>Game name</h1>
+      <button class="start-button">Start</button>
+    </section>`);
+
+    const startButton = document.querySelector('.start-button');
+    startButton.addEventListener('click', buildGameScreen);
+
+  }
+
+  function buildGameScreen() {
+    const title = updateTitle(`Game on!`);
+    const gameScreen = buildDom(`
+    <section>
+      <h1>Game screen</h1>
+      <canvas></canvas>
+    </section>`);
+
+    const canvasElement = document.querySelector('canvas');
+    const game = new Game(canvasElement);
+    game.startLoop();
+    game.setGameOverCallback(buildGameOverScreen);
+    game.setWinCallback(buildWinScreen);
+
+  }
+
+  function buildGameOverScreen() {
+    const title = updateTitle(`Game over`);
+    const gameOverScreen = buildDom(`
+    <section>
+      <h1>Game over!</h1>
+      <p>Lorum ipsum</p>
+      <button class="start-button">Play again</button>
+    </section>
+    `);
+
+    const startButton = document.querySelector('.start-button');
+    startButton.addEventListener('click', buildGameScreen);
+  }
+
+  function buildWinScreen() {
+    const title = updateTitle(`Winner!`);
+    const winScreen = buildDom(`
+    <section>
+      <h1>Winner winner chicken dinner!</h1>
+      <p>Lorum ipsum</p>
+      <button class="start-button">Play again</button>
+    </section>
+    `);
+
+    const startButton = document.querySelector('.start-button');
+    startButton.addEventListener('click', buildGameScreen);
+  }
+
+  buildSplash();
+
 }
 
-function updateTitle(name) {
-  document.title = name;
-}
-
-function buildSplash() {
-  const title = updateTitle(`Game name`);
-  const buildSplash = buildDom(`
-  <section>
-    <h1>Game name</h1>
-    <button class="start-button">Start</button>
-  </section>`);
-
-  const startButton = document.querySelector('.start-button');
-  startButton.addEventListener('click', buildGameScreen);
-
-}
-
-function buildGameScreen() {
-  const title = updateTitle(`Game on!`);
-  const gameScreen = buildDom(`
-  <section>
-    <h1>Game screen</h1>
-    <canvas></canvas>
-    <button class="game-over-button">Game over</button>
-    <button class="win-button">Win</button>
-  </section>`);
-
-  const gameOverButton = document.querySelector('.game-over-button'); // FOR TESTING - TO REMOVE
-  gameOverButton.addEventListener('click', buildGameOverScreen); // FOR TESTING - TO REMOVE
-  const winButton = document.querySelector('.win-button'); // FOR TESTING - TO REMOVE
-  winButton.addEventListener('click', buildWinScreen); // FOR TESTING - TO REMOVE
-
-}
-
-function buildGameOverScreen() {
-  const title = updateTitle(`Game over`);
-  const gameOverScreen = buildDom(`
-  <section>
-    <h1>Game over!</h1>
-    <p>Lorum ipsum</p>
-    <button class="start-button">Play again</button>
-  </section>
-  `);
-
-  const startButton = document.querySelector('.start-button');
-  startButton.addEventListener('click', buildGameScreen);
-}
-
-function buildWinScreen() {
-  const title = updateTitle(`Winner!`);
-  const winScreen = buildDom(`
-  <section>
-    <h1>Winner winner chicken dinner!</h1>
-    <p>Lorum ipsum</p>
-    <button class="start-button">Play again</button>
-  </section>
-  `);
-
-  const startButton = document.querySelector('.start-button');
-  startButton.addEventListener('click', buildGameScreen);
-}
-
-buildSplash();
+window.addEventListener('load', main);
