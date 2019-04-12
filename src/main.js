9bfce7a23ff9998b56a05e779ab8d79119be95fb
Scott Mallinson <scott@scottmallinson.com>
2019-04-12 14:51:47 +0200

    Created buildDom, buildSplash, buildGameScreen, buildGameOverScreen, buildWinScreen, addEventListener

--- src/main.js
+++ src/main.js
@@ -1,3 +1,66 @@
 'use strict';
 
 console.log('MAIN CONNECTED');
+
+const mainElement = document.querySelector('main');
+
+function buildDom(html) {
+  mainElement.innerHTML = html;
+  return mainElement;
+}
+
+function buildSplash() {
+  const buildSplash = buildDom(`
+  <section>
+    <h1>Game name</h1>
+    <button class="start-button">Start</button>
+  </section>`);
+
+  const startButton = document.querySelector('.start-button');
+  startButton.addEventListener('click', buildGameScreen);
+
+}
+
+function buildGameScreen() {
+  const gameScreen = buildDom(`
+  <section>
+    <h1>Game screen</h1>
+    <canvas></canvas>
+    <button class="game-over-button">Game over</button>
+    <button class="win-button">Win</button>
+  </section>`);
+
+  const gameOverButton = document.querySelector('.game-over-button'); // FOR TESTING - TO REMOVE
+  gameOverButton.addEventListener('click', buildGameOverScreen); // FOR TESTING - TO REMOVE
+  const winButton = document.querySelector('.win-button'); // FOR TESTING - TO REMOVE
+  winButton.addEventListener('click', buildWinScreen); // FOR TESTING - TO REMOVE
+
+}
+
+function buildGameOverScreen() {
+  const gameOverScreen = buildDom(`
+  <section>
+    <h1>Game over!</h1>
+    <p>Lorum ipsum</p>
+    <button class="start-button">Play again</button>
+  </section>
+  `);
+
+  const startButton = document.querySelector('.start-button');
+  startButton.addEventListener('click', buildGameScreen);
+}
+
+function buildWinScreen() {
+  const winScreen = buildDom(`
+  <section>
+    <h1>Winner winner chicken dinner!</h1>
+    <p>Lorum ipsum</p>
+    <button class="start-button">Play again</button>
+  </section>
+  `);
+
+  const startButton = document.querySelector('.start-button');
+  startButton.addEventListener('click', buildGameScreen);
+}
+
+buildSplash();
